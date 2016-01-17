67a14852f66cbcf82bb3fe47074d25b8086c62c3
Frizlab <fload@me.com>
2016-01-17 23:21:10 +0100

    Init

--- BSONSerialization/BSONSerialization.h
+++ BSONSerialization/BSONSerialization.h
@@ -1,19 +1,19 @@
-//
-//  BSONSerialization.h
-//  BSONSerialization
-//
-//  Created by François Lamboley on 1/17/16.
-//  Copyright © 2016 frizlab. All rights reserved.
-//
+/*
+ * BSONSerialization.h
+ * BSONSerialization
+ *
+ * Created by François Lamboley on 1/17/16.
+ * Copyright © 2016 frizlab. All rights reserved.
+ */
 
 #import <Cocoa/Cocoa.h>
 
-//! Project version number for BSONSerialization.
+/** Project version number for BSONSerialization. */
 FOUNDATION_EXPORT double BSONSerializationVersionNumber;
 
-//! Project version string for BSONSerialization.
+/** Project version string for BSONSerialization. */
 FOUNDATION_EXPORT const unsigned char BSONSerializationVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <BSONSerialization/PublicHeader.h>
 
+/* Imports like this: #import <BSONSerialization/PublicHeader.h> */
 
---
+++ BSONSerialization/BSONSerialization.swift
@@ -0,0 +1,14 @@
+/*
+ * BSONSerialization.swift
+ * BSONSerialization
+ *
+ * Created by François Lamboley on 1/17/16.
+ * Copyright © 2016 frizlab. All rights reserved.
+ */
+
+import Cocoa
+
+
+
+class BSONSerialization {
+}
