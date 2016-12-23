1450972f4dd7e8673fd26eb887ff0036618c0431
Frizlab <fload@me.com>
2016-12-24 00:17:04 +0100

    Indent commit

--- BSONSerialization/BSONClasses.swift
+++ BSONSerialization/BSONClasses.swift
@@ -182,8 +182,8 @@ func areBSONDocEqual(_ doc1: BSONDoc, _ doc2: BSONDoc) throws -> Bool {
 
 private func areBSONEntitiesEqual(_ entity1: Any?, _ entity2: Any?) throws -> Bool {
 	switch entity1 {
-	case nil:                  guard entity2 == nil else {return false}
-	case let val as Bool:      guard entity2 as? Bool      == val else {return false}
+	case nil:                  guard entity2          == nil else {return false}
+	case let val as Bool:      guard entity2 as? Bool == val else {return false}
 		
 	case let val1 as Int32:
 		guard entity2 as? Int32 == val1 else {
