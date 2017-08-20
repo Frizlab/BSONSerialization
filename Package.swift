c16c50e1bd30f45b32be652635ecbca34f724b00
Frizlab <fload@me.com>
2017-08-20 19:16:20 +0200

    Migrate BufferStream to an external dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,25 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "BSONSerialization"
+	name: "BSONSerialization",
+	products: [
+		.library(
+			name: "BSONSerialization",
+			targets: ["BSONSerialization"]
+		)
+	],
+	dependencies: [
+		.package(url: "https://github.com/Frizlab/SimpleStream", from: "1.0.0"),
+	],
+	targets: [
+		.target(
+			name: "BSONSerialization",
+			dependencies: ["SimpleStream"]
+		),
+		.testTarget(
+			name: "BSONSerializationTests",
+			dependencies: ["BSONSerialization"]
+		)
+	]
 )
